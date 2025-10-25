230ea292f30f19e455fab10f777c526386b84db9
shennawy <ahmedshennawy997@gmail.com>
2025-10-26 02:58:26 +0300

    phone screen

--- public/Components/ScreenError/ScreenError.tsx
+++ public/Components/ScreenError/ScreenError.tsx
@@ -3,18 +3,14 @@ import { RiErrorWarningFill } from "react-icons/ri";
 export default function ScreenError() {
   return (
     <>
-      <div className="container col-12 text-center text-white">
-        <div className="inner p-3">
-          <h1>
-            This application requires a larger screen. Please access it from a
-            desktop or laptop computer.
-          </h1>
-          <hr />
-          <h1 className="mb-3">
-            يتطلب هذا الموقع شاشه اكبر لاستخدامه بشكل أفضل
-          </h1>
-          <RiErrorWarningFill />
-        </div>
+      <div className="inner col-12 text-center text-white m-0 p-3 m-0">
+        <h4>
+          The website requires a larger screen. Please access it from a desktop
+          or laptop computer.
+        </h4>
+        <hr />
+        <h4 className="mb-3">يتطلب الموقع شاشه اكبر لإستخدامه بشكل أفضل</h4>
+        <RiErrorWarningFill />
       </div>
     </>
   );
